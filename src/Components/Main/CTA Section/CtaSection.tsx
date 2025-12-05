ac02b8d3f26f55e1306337e70d85eca5e5b2f10c
Gaga <lalisa.tamene@aastustudent.edu.et>
2025-12-05 07:23:18 +0300

    working on CTA section

---
+++ src/Components/Main/CTA Section/CtaSection.tsx
@@ -0,0 +1,8 @@
+import image1 from "../../../assets/image1.avif";
+const CtaSection = () => {
+  return (
+    <img src={image1} alt="image"/>
+  )
+}
+
+export default CtaSection;
--- src/Components/Main/Main.tsx
+++ src/Components/Main/Main.tsx
@@ -1,7 +1,10 @@
 import Button from "../../Components/Shared/Button/Button";
-
+import CtaSection from "../../Components/Main/CTA Section/CtaSection";
 export default function Main() {
   return (
-    <Button strokeClr="outline-accent1" textClr="text-background2" background="bg-accent1" />
+    <>
+      <Button strokeClr="outline-accent1" textClr="text-background2" background="bg-accent1" />
+      <CtaSection />
+    </>
   );
 }
