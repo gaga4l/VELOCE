77e78e18ababc930e9d9bf2a1794ee4792280031
Gaga <lalisa.tamene@aastustudent.edu.et>
2025-12-05 10:05:16 +0300

    working on CTA section

--- src/Components/Main/CTA Section/CtaSection.tsx
+++ src/Components/Main/CTA Section/CtaSection.tsx
@@ -1,7 +1,16 @@
-import image1 from "../../../assets/image1.avif";
+import image1 from "../../../assets/image1.avif"
+import Button from "../../Shared/Button/Button";
 const CtaSection = () => {
   return (
-    <img src={image1} alt="image"/>
+    <section className="relative">
+      <img className="w-fit h-[536px]" src={image1} alt="image1" />
+      <div className="flex flex-col absolute top-0 w-fit h-fit bg-background2 p-[30px] gap-[60px]">
+        <p className="font-staatliches text-[32px]">The only SPIRIT-FREE aperitif that tastes as good as it makes you feel.</p>
+        <Button textClr="text-background2" strokeClr="outline-accent1" background="bg-accent1" />
+      </div>
+      
+    </section>
+
   )
 }
 
--- src/Components/Main/Main.tsx
+++ src/Components/Main/Main.tsx
@@ -1,9 +1,7 @@
-import Button from "../../Components/Shared/Button/Button";
 import CtaSection from "../../Components/Main/CTA Section/CtaSection";
 export default function Main() {
   return (
     <>
-      <Button strokeClr="outline-accent1" textClr="text-background2" background="bg-accent1" />
       <CtaSection />
     </>
   );
