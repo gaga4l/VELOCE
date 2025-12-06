fd064d93e7a2a94caace6ce0e2d4769737056603
Gaga <lalisa.tamene@aastustudent.edu.et>
2025-12-06 07:50:01 +0300

    added a hover effect for products

--- src/Components/Main/Product Grid/Product/Product.tsx
+++ src/Components/Main/Product Grid/Product/Product.tsx
@@ -8,16 +8,25 @@ const Product = ({ name, description, image }: Props) => {
     <article className="w-full h-[543px] flex flex-col gap-[30px] pb-[50px]">
       <div
         className="w-full relative aspect-3/3 px-[30px] py-[30px] bg-cover bg-center"
-        style={{backgroundImage: `url(${image})`, backgroundPosition: 'center', backgroundSize: 'cover'}}
-      >
-        <p className="absolute font-staatliches text-[32px] text-[#ffffff] leading-[120%] tracking-[1%]">
+        style={{
+          backgroundImage: `url(${image})`,
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+        }}
+      >   
+          <div className="absolute  inset-0 bg-white opacity-0 hover:opacity-30 transition-opacity duration-300"></div>
+        <p className="absolute z-10 font-staatliches text-[32px] text-[#ffffff] leading-[120%] tracking-[1%]">
           {name}
         </p>
+       
       </div>
       <p className="font-staatliches text-[18px] text-body1 leading-[145%] tracking-[1%]">
         {description}
       </p>
-      <a href="#" className="font-staatliches text-[18px] text-body1 leading-[145%] tracking-[1%]">
+      <a
+        href="#"
+        className="font-staatliches text-[18px] text-body1 leading-[145%] tracking-[1%] hover:opacity-60 transition-opacity duration-300"
+      >
         Find near you
       </a>
     </article>
