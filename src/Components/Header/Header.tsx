9ac9b24480f3b3ec505f24b48a758ea870bbdd07
Gaga <lalisa.tamene@aastustudent.edu.et>
2025-12-06 13:27:51 +0300

    Finished responsivness of header for medium size screens

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -8,7 +8,7 @@ const Header  = () => {
     <header>
       <div className="flex justify-between pl-10 pr-8 pt-3 pb-6 md:flex-col md:justify-start md:gap-[70px] md:p-[30px]">
         <Veloce />
-        <div className="pr-[30px] md:p-0 md:order-first md:flex md:flex-col md:gap-[50px] ">
+        <div className="pr-[30px] md:p-0 md:order-first md:flex md:flex-col md:gap-[50px]">
           <Tagline />
           <NavLink />
         </div>
--- src/Components/Header/Navigation/WordMark/Veloce.tsx
+++ src/Components/Header/Navigation/WordMark/Veloce.tsx
@@ -5,12 +5,11 @@ import VeloceDesktop from "../../../../assets/Veloce Desktop.svg";
 export default function Veloce() {
   return (
     <div>
-      <a href="/" className="block md:hidden">
+      <a href="/" className="block md:hidden ">
         <img src={VeloceMobile} alt="Wordmark" className="w-full" />
       </a>
       <picture className="hidden md:block">
         <source media="(min-width: 1024px)" srcSet={VeloceDesktop} />
-        <source media="(min-width: 768px)" srcSet={VeloceTablet} />
         <img src={VeloceTablet} alt="Wordmark" className="w-full" />
       </picture>
     </div>
