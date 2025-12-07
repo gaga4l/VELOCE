176b85b6b1d10e52eb3b9e5bb9df70c7051c0396
Gaga <lalisa.tamene@aastustudent.edu.et>
2025-12-07 11:23:06 +0300

    Finished  Featured product section responsivness for all screen sizes

--- src/Components/Main/Featured Product/FeaturedProduct.tsx
+++ src/Components/Main/Featured Product/FeaturedProduct.tsx
@@ -5,15 +5,16 @@ const FeaturedProduct = () => {
     <section style={{
       backgroundImage: `url(${FeatureProductImage})`
     }} 
-    className="md:h-[800px] md:w-full bg-cover bg-center"
+    className="h-[800px] md:w-full bg-cover bg-center"
     > 
         {/*<div className="absolute inset-0 bg-black/40"></div>*/}
-        <div className="backdrop-brightness-75 w-full h-full md:px-[94px] md:py-20 md:flex md:items-end md:justify-center ">
-          <div className="md:w-[509px] md:h-fit md:p-0 md:flex md:flex-col md:gap-2">
-            <div className="md:w-[115px] md:h-11"><img src={NewTag} alt="New Tag" /></div>
-            <div className="md:w-fit md:h-fit md:text-center md:flex md:flex-col md:gap-2 ">
-              <p className="font-staatliches text-headline2 md:text-[130px] md:leading-[80%] md:tracking-[1%] md:text-left">Primavera</p>
-              <p className="font-staatliches text-body2 md:w-full md:text-[24px] md:leading-[135%] md:tracking-[1%]">A bold, spiced aperitif featuring cardamom, ginger, and cinnamon.</p>
+        <div className="backdrop-brightness-75 w-full h-full flex justify-center gap-2 px-11 py-20 md:px-[94px] md:items-end lg:px-[145px]">
+          
+          <div className="h-full w-[287px] flex flex-col items-center justify-between md:w-[509px] md:items-start md:h-fit md:p-0 md:gap-2 lg:w-fit lg:h-fit">
+            <div className="w-[115px] h-11"><img src={NewTag} alt="New Tag" /></div>
+            <div className="text-center flex flex-col gap-2 px-[33px] md:p-0 md:w-fit md:h-fit">
+              <p className="font-staatliches text-headline2 text-[100px] leading-[80%] tracking-[1%] break-all md:text-[130px]  md:text-left lg:text-[150px]">Primavera</p>
+              <p className="font-staatliches text-body2 md:w-full text-[22px] leading-[145%] w-[221px] tracking-[1%] md:text-[24px] md:leading-[135%]  lg:text-[30px] lg:w-[584px]">A bold, spiced aperitif featuring cardamom, ginger, and cinnamon.</p>
             </div>
           </div>
         </div>
