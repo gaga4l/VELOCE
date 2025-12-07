61adcb919db1fb9c36893a87e72a988bba4edd07
Gaga <lalisa.tamene@aastustudent.edu.et>
2025-12-07 15:18:03 +0300

    Fixed the bug on stocklist section, made the screen size for xl rather than lg and solve the responsivness issue on medium size screens

--- src/Components/Main/Stocklist Section/StocklistSection.tsx
+++ src/Components/Main/Stocklist Section/StocklistSection.tsx
@@ -1,14 +1,14 @@
 import StocklistImage from "../../../assets/stocklistist image.jpg";
 const StocklistSection = () => {
   return (
-    <section className="w-full h-fit md:h-[230px] lg:h-[640px] lg:flex lg:flex-row">
-      <div className="w-full h-fit px-[9px] py-[23px] bg-background1 md:px-0">
-        <p className="font-staatliches text-headline1 text-[32px] leading-[120%] tracking-[1%] text-center md:text-[38px] md:leading-[110%] lg:[580px] lg:text-[42px] ">
+    <section className="w-full h-fit xl:h-[640px] xl:flex xl:flex-row">
+      <div className="w-full h-fit px-[9px] py-[23px] md:h-[230px] bg-background1 md:px-0 md:flex md:justify-center">
+        <p className="font-staatliches text-headline1 text-[32px] leading-[120%] tracking-[1%] text-center md:text-[38px] md:leading-[110%] md:w-[740px] md:text-left xl:w-[580px] xl:text-[42px] ">
           Experience the refined taste of Véloce. Visit our premium stockists
           for your next alcohol-free aperitif.
         </p>
       </div>
-      <img className="w-full h-[375px] object-cover object-center md:h-[470px] lg:h-[640px]" src={StocklistImage} alt="stocklist image" />
+      <img className="w-full h-[375px] object-cover object-center md:h-[470px] xl:h-[640px]" src={StocklistImage} alt="stocklist image" />
     </section>
   );
 };
