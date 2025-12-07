f17f560feff85ec38139c37001e45f8180a4cd0f
Gaga <lalisa.tamene@aastustudent.edu.et>
2025-12-07 10:17:11 +0300

    working on Featured product section responsivness

--- src/Components/Main/Featured Product/FeaturedProduct.tsx
+++ src/Components/Main/Featured Product/FeaturedProduct.tsx
@@ -2,29 +2,23 @@ import NewTag from "../../../assets/Circular Tag.svg";
 import FeatureProductImage from "../../../assets/maxim-hopman-NLCKduPjJqo-unsplash.jpg";
 const FeaturedProduct = () => {
   return (
-    <section>
-      <div
-        style={{
-          backgroundImage: `url(${FeatureProductImage})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}
-      >
-        <div className="backdrop-brightness-75 w-full h-[800px] px-11 py-20">
-          <div className="flex flex-col  md:px-[94px] md:py-20 justify-between items-start h-full md:justify-end md:max-w-[612px]">
-            <img className="md:w-[115px] md:h-11" src={NewTag} alt="New Tag" />
-          <div className="text-center px-[33px]  flex flex-col gap-2 w-[287px] items-center justify-center md:w-fit md:p-0 md:text-center">
-            <p className="font-staatliches  wrap-break-word text-[100px]  text-headline2 leading-[80%] tracking-[1%] break-all md:text-[130px] md:break-normal">
-              Primavera
-            </p>
-            <p className="font-staatliches text-[22px] text-body2 leading-[145%] tracking-[1%] md:text-[24px] md:leading-[135%]">
-              A bold, spiced aperitif featuring cardamom, ginger, and cinnamon.
-            </p>
+    <section style={{
+      backgroundImage: `url(${FeatureProductImage})`,
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+    }} 
+    className="md:h-[800px] md:w-full"
+    > 
+        <div className="backdrop-brightness-75 w-full h-full md:px-[94px] md:py-20 md:flex md:items-end md:justify-center ">
+          <div className="md:w-[509px] md:h-fit md:p-0 md:flex md:flex-col md:gap-2">
+            <div className="md:w-[115px] md:h-11"><img src={NewTag} alt="New Tag" /></div>
+            <div className="md:w-fit md:h-fit md:text-center md:flex md:flex-col md:gap-2 ">
+              <p className="font-staatliches text-headline2 md:text-[130px] md:leading-[80%] md:tracking-[1%] md:text-left">Primavera</p>
+              <p className="font-staatliches text-body2 md:w-full md:text-[24px] md:leading-[135%] md:tracking-[1%]">A bold, spiced aperitif featuring cardamom, ginger, and cinnamon.</p>
+            </div>
           </div>
-          </div>
-          
         </div>
-      </div>
+  
     </section>
   );
 };
