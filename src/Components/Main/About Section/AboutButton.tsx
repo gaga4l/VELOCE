99d893c82859f9fdb3b6a73a6d3dfd931c097645
Gaga <lalisa.tamene@aastustudent.edu.et>
2025-12-07 07:05:48 +0300

    Made the About section responsive for big screen sizes

--- src/Components/Main/About Section/AboutButton.tsx
+++ src/Components/Main/About Section/AboutButton.tsx
@@ -1,7 +1,7 @@
 const ShopNowButton = () => { 
   return (
-    <div className={"bg-background1 outline-[1.3px] outline-[#000000] font-jaro text-accent1  hover:bg-[#000000]/10 transition-opacity duration-300 w-fit h-fit px-[26px] py-[7.8px] rounded-[50%/50%] flex align-middle justify-center"}>
-      <button className="md:text-[24px] md:leading-[139%] md:tracking-[-1%]">
+    <div className="bg-background1 outline-[1.3px] outline-[#000000] font-jaro text-accent1  hover:bg-[#000000]/10 transition-opacity duration-300 w-fit h-fit px-[26px] py-[7.8px] rounded-[50%/50%] flex align-middle justify-center lg:inset-0">
+      <button className="md:text-[24px] md:leading-[139%] md:tracking-[-1%] lg:text-[26px]">
         About Us
       </button>
     </div>
--- src/Components/Main/About Section/AboutSection.tsx
+++ src/Components/Main/About Section/AboutSection.tsx
@@ -1,9 +1,9 @@
 import AboutButton from "./AboutButton";
 const AboutSection = () => {
   return (
-    <section>
-      <div className="w-full h-fit bg-background1 px-5 pt-[30px] pb-[100px] flex flex-col gap-[50px] md:px-[30px]">
-        <p className="font-staatliches text-[40px] leading-[110%] tacking-[-0.5%] md:text-[50px] md:tracking-[-1%]">
+    <section className="bg-background1 px-5 pt-[30px] pb-[100px] md:px-[30px]">
+      <div className="w-full h-fit flex flex-col gap-[50px]">
+        <p className="font-staatliches text-[40px] leading-[110%] tacking-[-0.5%] md:text-[50px] md:tracking-[-1%] lg:text-[60px]">
           Véloce is the ZERO-PROOF aperitif for modern living, crafted with super
           herbs and nutraceuticals to elevate your well-being, without
           compromising the sophisticated ritual of the aperitif.
