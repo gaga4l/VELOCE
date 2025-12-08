71705284ebf6396dea2a54c743db170a6899ad0a
Gaga <lalisa.tamene@aastustudent.edu.et>
2025-12-08 11:59:47 +0300

    Finished About page founder section left content structure, style and responsivness for all screen sizes

--- src/Pages/About/Components/Main/FounderSection/FounderSection.tsx
+++ src/Pages/About/Components/Main/FounderSection/FounderSection.tsx
@@ -1,10 +1,13 @@
 import RecognitionList from "./RecognitionList";
 const FounderSection = () => {
   return (
-    <section className="grid h-[1537px] md:h-[1280px] lg:grid-cols-2 lg:h-[800px] ">
-      <div className="bg-background2 flex md:h-[640px]  lg:flex-col gap-[41px] items-start px-[30px] py-10  ">
+    <section className="grid h-[1537px] md:h-[1280px] lg:grid-cols-2 lg:h-[800px]">
+      <div className="bg-background2 h-fit flex flex-col gap-[41px] items-start px-[30px] py-10 md:h-[640px] lg:h-full">
         <p className="font-geist font-bold text-headline1 text-[13px] leading-[110%] tracking-[-5%] md:text-[14px] lg:text-[15px] ">RECOGNITION</p>
         <RecognitionList />
+      </div>
+      <div>
+        
       </div>
     </section>
   )
--- src/Pages/About/Components/Main/FounderSection/RecognitionList.tsx
+++ src/Pages/About/Components/Main/FounderSection/RecognitionList.tsx
@@ -31,10 +31,10 @@ const listData = [
 
 const RecognitionList = () => {
   return (
-    <ul>
+    <ul className="w-full h-fit">
       {listData.map(data => {
         return (
-          <li className="font-staatliches  text-headline1 text-[22px] pt-4 pb-2.5 leading-[145%] tracking-[1%] outline md:text-[24px] md:leading-[135%] lg:leading-[130%] lg:text-[30px]" ><span className="font-geist font-bold leading-[115%] tracking-[-5%] text-[13px] md:text-[14px] lg:text-[15px] lg:mr-[30px]  ">{data.year}</span>{data.Recognition}</li>
+          <li className="font-staatliches flex gap-2.5 flex-col border-b text-headline1 text-[22px] pt-5 pb-2.5 md:flex-row md:items-center md:gap-[30px] md:pt-4 leading-[145%] tracking-[1%] md:text-[24px] md:leading-[135%] lg:leading-[130%] lg:text-[30px]"><span className="font-geist font-bold leading-[115%] tracking-[-5%] text-[13px] md:text-[14px] lg:text-[15px]">{data.year}</span>{data.Recognition}</li>
         )
       })}
     </ul>
