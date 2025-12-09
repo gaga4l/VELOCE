08eaac369c4b9e31ea5df7a568a18a28b0f7d6b0
Gaga <lalisa.tamene@aastustudent.edu.et>
2025-12-09 10:08:58 +0300

    Working on about page contact section

--- src/Pages/About/Components/Main/ContactInfo.tsx
+++ src/Pages/About/Components/Main/ContactInfo.tsx
@@ -1,7 +1,44 @@
+import MacroMontero from "../../../../assets/marco-montero-pisani-5qTxX7nicco-unsplash.jpg"
+
+const contactData = [
+  {
+    id: 1,
+    contact: "Phone",
+    value: "+1 (555) 123-4567"
+  },
+  {
+    id: 2,
+    contact: "Email",
+    value: "info@veloce.com"
+  },
+  {
+    id: 3,
+    contact: "Social",
+    value: "@veloce"
+  }
+]
+
 const ContactInfo = () => {
   return (
   <section className="grid h-[1537px] md:h-[1280px] xl:grid-cols-2 lg:h-[800px]">
-  
+    <div className="relative h-[800px] md:h-[640px] lg:h-[800px] ">
+      <img className="object-cover object-center h-full w-full saturate-0" src={MacroMontero} alt="Macro Montero Image" />
+    </div>
+    <div className="p-[30px] md:h-[800px] flex flex-col gap-[260px] md:justify-between ">
+      <div className="flex flex-col gap-6">
+        <p className="font-geist text-headline2 font-bold leading-[110%] tracking-[-5%] lg:text-[15px] ">CONTACT</p>
+        <ul className="flex flex-col gap-6">
+          {contactData.map(data => {
+            return (
+              <li className="font-staatliches text-headline2 text-[22px] leading-[145%] tracking-[1%] md:text-[24px] md:leading-[135%] lg:leading-[130%] lg:text-[30px] flex justify-between" key={data.id}>
+                <span>{data.contact}</span> {data.value}
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+      
+    </div>
   </section>
   )
 }
--- src/Pages/About/Components/Main/FounderSection/FounderSection.tsx
+++ src/Pages/About/Components/Main/FounderSection/FounderSection.tsx
@@ -5,7 +5,7 @@ import DecorativeBorder from "../../../../../Components/Header/DecorativeBorder"
 const FounderSection = () => {
   return (
     <section className="grid h-[1537px] md:h-[1280px] xl:grid-cols-2 lg:h-[800px]">
-      <div className="bg-background2 h-fit order-2 flex flex-col gap-[41px] items-start px-[30px] py-10 md:h-[640px] lg:h-full xl:order-1">
+      <div className="bg-background2 h-fit order-last flex flex-col gap-[41px] items-start px-[30px] py-10 md:h-[640px] lg:h-full xl:order-first">
         <p className="font-geist font-bold text-headline1 text-[13px] leading-[110%] tracking-[-5%] md:text-[14px] lg:text-[15px] ">
           RECOGNITION
         </p>
